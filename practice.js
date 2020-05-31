523979fbd3ce95c01d6431ad0d310d6e521c90f2
Aneesh Vempaty <aneesh.vempaty@gmail.com>
2020-05-30 21:20:40 -0700

    Full workflow completed, issue with promises

--- practice.js
+++ practice.js
@@ -9,21 +9,24 @@ const options = {
 
     headers: {
         'Content-type': 'application/json',
-        'accept': 'application/json'
+        'accept': 'application/json',
+        'authorization': ' Bearer Av3iqd87K8RFVJ3RiS2q33hiw27EAtt-ahG9'
     },
     /*auth: {
         'sendImmediately': true,
-        'bearerToken': 'ejxq-U52GRFGE-slYCd-lqXrulsWpXUME_PE'
+       
     },*/
-
-    first_name: 'a',
+    first_name: 'aneesh',
     last_name: 'vempaty',
-    email: 'adity.vempaty@gmail.com',
+    email: 'dummy@gmail.com',
     gender: 'male'
 
 }
 
-makeRequest('https://gorest.co.in/public-api/users', 'POST', options).then((resp) => {
+//var id = ''
+
+makeRequest('https://gorest.co.in/public-api/users', 'POST', options)
+.then((resp) => {
     if (resp.statusCode === 502) {
         console.log('Received 502 error')
         return
@@ -36,21 +39,91 @@ makeRequest('https://gorest.co.in/public-api/users', 'POST', options).then((resp
         console.log('Received 40X error')
         return
     } else if (resp.statusCode === 200) {
-        console.log(resp.body)
+        console.log('Received 200')
+        console.log(resp)
     }
+    //console.log(resp.statusCode)
+    console.log(resp.body)
+    var id = resp.body.result.id
+    return id
 })
+.then((id) => {
+    makeRequest('https://gorest.co.in/public-api/users/' + id, 'GET', {})
+    .then((resp) => {
+        if (resp.statusCode === 502) {
+            throw new Error('Received 502 error at GET ')
+        }
+        if (resp.statusCode === 500) {
+            throw new Error('Received 502 error at GET ')
+
+        }
+        if (resp.statusCode >= 400) {
+            throw new Error('Received 40X error at GET ')
+        } else if (resp.statusCode === 200) {
+            console.log('Received 200 at GEt')
+            //console.log(resp)
+        }
+        console.log(id)
+        console.log(resp.body)
+    })
+    return id
+})
+.then((id) => {
+    makeRequest('https://gorest.co.in/public-api/users/' + id, 'PUT', { website: "http://google.com" }).then((resp) => {
+        if (resp.statusCode === 502) {
+            throw new Error('Received 502 error at PUT ')
+        }
+        if (resp.statusCode === 500) {
+            throw new Error('Received 502 error at PUT ')
+        }
+        if (resp.statusCode >= 400) {
+            throw new Error('Received 40X error at PUT ')
+        } else if (resp.statusCode === 200) {
+            console.log('Received 200 at PUT')
+            //console.log(resp)
+        }
+        console.log(id)
+        console.log(resp.statusCode)
+        console.log(resp.body)
+    })
+    return id
+})
+.then((id) => {
+    makeRequest('https://gorest.co.in/public-api/users/' + id, 'DELETE', {}).then((resp) => {
+        if (resp.statusCode === 502) {
+            throw new Error('Received 502 error at DELETE ')
+        }
+        if (resp.statusCode === 500) {
+            throw new Error('Received 502 error at DELETE ')
+        }
+        if (resp.statusCode >= 400) {
+            throw new Error('Received 40X error at DELETE ')
+        } else if (resp.statusCode === 200) {
+            console.log('Received 200 at DELETE')
+            //console.log(resp)
+        }
+        console.log(id)
+        console.log(resp.statusCode)
+        console.log(resp.body._meta.success)
+    })
+})
+    .catch((err) => {
+        console.log(err.statusCode)
+        console.log(err.message)
+    })
 
 function makeRequest(url, method, body) {
     return new Promise((resolve, reject) => {
         request(url, { method: method, json: body || true }, (err, res) => {
-            console.log(body)
+            //console.log(body)
             //console.log(res)
             if (err) {
                 reject(err)
                 return
             }
             resolve(res)
-        }).auth(null, null, true, 'ejxq-U52GRFGE-slYCd-lqXrulsWpXUME_PE')
+        })
+        .auth(null, null, true, 'wLejCijFBsVpo8gKfXH-JAqN_slWt3x6tOVI')
     })
 }
 
